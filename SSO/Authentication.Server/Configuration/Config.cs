795bbb40578a8110524d2d5c547564827345de0a
Marat <marat_17.5@mail.ru>
2019-01-11 18:04:16 +0400

    add project

---
+++ SSO/Authentication.Server/Configuration/Config.cs
@@ -0,0 +1,67 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Authentication.Server.Configuration
+{
+    public  class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+             {
+                  new ApiResource("api1", "My Api"),
+                  new ApiResource("mvc1", "Mvc1"),
+                  new ApiResource("mvc2","Mvc2")
+             };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "App1",
+                    ClientName = "Application1",
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                },
+
+                new Client
+                {
+                    ClientId = "App2",
+                    ClientName = "Application2",
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
---
+++ SSO/Authentication.Server/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Authentication.Server.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Authentication.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddIdentityServer()
+                   .AddDeveloperSigningCredential()
+                   .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                   .AddInMemoryApiResources(Config.GetApiResources())
+                   .AddInMemoryClients(Config.GetClients());
+                  // .AddTestUsers(Config.GetUsers());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseIdentityServer();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Authentication Server");
+            });
+        }
+    }
+}
